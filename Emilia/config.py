4b8466dc24145e1cf1b13edd0bf1eafa85932be0
𝗖ʜᴏᴄᴏ <chocoXgithub@gmail.com>
2024-11-16 18:21:15 +0530

    Update config.py

--- Emilia/config.py
+++ Emilia/config.py
@@ -8,25 +8,25 @@ def get_user_list(config, key):
 
 
 class Config(object):
-    API_HASH = "45aabfac" # API_HASH from my.telegram.org
-    API_ID = 62 # API_ID from my.telegram.org
+    API_HASH = "3fc2b371f4fbb0166758736414d8be92" # API_HASH from my.telegram.org
+    API_ID = 25475489 # API_ID from my.telegram.org
 
-    BOT_ID = 521 # BOT_ID
-    BOT_USERNAME = "Elf_Robot" # BOT_USERNAME
+    BOT_ID = 7617090520 # BOT_ID
+    BOT_USERNAME = "Fubuki_cutibot" # BOT_USERNAME
 
-    MONGO_DB_URL = "mongodb://username:password@localhost:27017/emi?directConnection=true&authSource=admin" # MongoDB URL from MongoDB Atlas
+    MONGO_DB_URL = "mongodb+srv://1:1@cluster0.ull4r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" # MongoDB URL from MongoDB Atlas
 
-    SUPPORT_CHAT = "SpiralTechDivision" # Support Chat Username
-    UPDATE_CHANNEL = "SpiralUpdates" # Update Channel Username
+    SUPPORT_CHAT = "TheSuperiorDynasty" # Support Chat Username
+    UPDATE_CHANNEL = "Choco_for_u" # Update Channel Username
     START_PIC = "https://pic-bstarstatic.akamaized.net/ugc/9e98b6c8872450f3e8b19e0d0aca02deff02981f.jpg@1200w_630h_1e_1c_1f.webp" # Start Image
-    DEV_USERS = [6040984893, 6461051572, 7107018652] # Dev Users
-    TOKEN = "57375" # Bot Token from @BotFather
+    DEV_USERS = [7512760579, 6940098429, 1266240012] # Dev Users
+    TOKEN = "7617090520:AAH6OqlzkAGlJUhtd6O3yd9sRfDo6lxwn00" # Bot Token from @BotFather
 
-    EVENT_LOGS = -10093 # Event Logs Chat ID
-    OWNER_ID = 6040984893 # Owner ID
+    EVENT_LOGS = -1002122538649 # Event Logs Chat ID
+    OWNER_ID = 1266240012 # Owner ID
  
     TEMP_DOWNLOAD_DIRECTORY = "./" # Temporary Download Directory
-    BOT_NAME = "Emilia" # Bot Name
+    BOT_NAME = "˹𝙁ᴜʙᴜᴋɪ ✘ 𝐌ᴜsɪᴄ˼🫧" # Bot Name
     WALL_API = "6950f53" # Wall API from wall.alphacoders.com
     ORIGINAL_EVENT_LOOP = True # Do not Change
 
